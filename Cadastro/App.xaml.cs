f97fd0a202d5efe78f42f5cf39224037b0b448bc
migue <migue@DESKTOP-C38H3HC>
2024-11-14 17:14:27 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Cadastro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
